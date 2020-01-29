99a7793ccf753535947d1bef089148a3dfb93faa
Timur Mishiev <timurmishiev@gmail.com>
2020-01-29 02:26:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
 
+# method oxford_comma takes an argument array of string elements 
+# and converts it into a string using the Oxford comma.
+def oxford_comma(array)
+   if array.length == 1
+      #join method convers array's elements to string
+      array.join
+   elsif array.length == 2
+      array.join(" and ")
+   else
+      #take the last element out of the array and save it for later
+      lastStr = array.pop
+      #join with "," and bring the last element back so we can do ", and" at the end
+      array.join(", ") << ", and #{lastStr}"
+      #return "#{array.join(", ")}, and #{lastStr}"
+   end
 end
