93f121d617c2d1d0f1a707ec885e3534d2a950ea
Timur Mishiev <timurmishiev@gmail.com>
2020-01-29 02:39:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -16,20 +16,3 @@ def oxford_comma(array)
       #return "#{array.join(", ")}, and #{lastStr}"
    end
 end
-=======
-
-array = ["kiwi"]
-def oxford_comma(array)
-  if array.length == 2
-  array[-2] << " and "
-  array.join
-  elsif array.length == 1
-  	array.join
-  elsif array.length > 2
-    array[-1].prepend "and "
-	array.join(", ")
-  end
-end
-
-oxford_comma(array)
->>>>>>> 807f68a504b21037ffbdaf7cded33cb1a730c3e0
